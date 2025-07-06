fd14cd46f066e49b4a1861d14cd960e1d90cf398
Maryori Nolberto <maryorithaisnolbertobautista@gmail.com>
2025-07-06 17:13:20 -0500

    🔄 Migración de flask_mysqldb a pymysql para compatibilidad con Railway

--- db.py
+++ db.py
@@ -1,12 +1,13 @@
-import pymysql
 import os
+from flask_mysqldb import MySQL
 
-def get_connection():
-    return pymysql.connect(
-        host=os.getenv('MYSQL_HOST').strip(),
-        user=os.getenv('MYSQL_USER'),
-        password=os.getenv('MYSQL_PASSWORD'),
-        database=os.getenv('MYSQL_DB'),
-        port=int(os.getenv('MYSQL_PORT', 3306)),
-        cursorclass=pymysql.cursors.DictCursor
-    )
+mysql = MySQL()
+
+def init_db(app):
+    app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST').strip()
+    app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+    app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+    app.config['MYSQL_PORT'] = int(os.getenv('MYSQL_PORT', 3306))
+    app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
+    app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+    mysql.init_app(app)
