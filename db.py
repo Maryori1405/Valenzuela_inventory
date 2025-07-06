3be3e880d7f0559563a5264b8f06ecaf6b1306a7
Maryori Nolberto <maryorithaisnolbertobautista@gmail.com>
2025-07-06 16:33:40 -0500

    🔧 Fix conexión MySQL usando variables de entorno

--- db.py
+++ db.py
@@ -1,12 +1,13 @@
+import os
 from flask_mysqldb import MySQL
 
 mysql = MySQL()
 
 def init_db(app):
-    app.config['MYSQL_HOST'] = 'yamanote.proxy.rlwy.net '
-    app.config['MYSQL_USER'] = 'root'      # Cambia si usas otro usuario
-    app.config['MYSQL_PASSWORD'] = 'root'      # Cambia si tienes contraseña
-    app.config['MYSQL_PORT'] = 3306
-    app.config['MYSQL_DB'] = 'inventario_db'
-    app.config['MYSQL_CURSORCLASS'] = 'DictCursor'  # Para obtener dicts y no tuplas
+    app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+    app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+    app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+    app.config['MYSQL_PORT'] = int(os.getenv('MYSQL_PORT', 3306))
+    app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
+    app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
     mysql.init_app(app)
