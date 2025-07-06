82a9e8efd45fabf3931b1df0a9995d30ea466da8
Maryori Nolberto <maryorithaisnolbertobautista@gmail.com>
2025-07-06 16:23:07 -0500

    🔧 Configuración de conexión MySQL para Render

--- db.py
+++ db.py
@@ -3,10 +3,10 @@ from flask_mysqldb import MySQL
 mysql = MySQL()
 
 def init_db(app):
-    app.config['MYSQL_HOST'] = 'localhost'
+    app.config['MYSQL_HOST'] = 'yamanote.proxy.rlwy.net '
     app.config['MYSQL_USER'] = 'root'      # Cambia si usas otro usuario
     app.config['MYSQL_PASSWORD'] = 'root'      # Cambia si tienes contraseña
-    app.config['MYSQL_PORT'] = 3307
+    app.config['MYSQL_PORT'] = 3306
     app.config['MYSQL_DB'] = 'inventario_db'
     app.config['MYSQL_CURSORCLASS'] = 'DictCursor'  # Para obtener dicts y no tuplas
     mysql.init_app(app)
