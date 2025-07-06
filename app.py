3ec0a35deaad4d7cabc780bea696dc53e5e97630
Maryori Nolberto <maryorithaisnolbertobautista@gmail.com>
2025-07-06 16:43:16 -0500

    🔧 Corrijo datos de conexión para Railway

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from sklearn.linear_model import LinearRegression
 import io, base64, unicodedata, matplotlib
 import matplotlib.pyplot as plt
 from dotenv import load_dotenv
+from db import init_db, mysql
 import numpy as np
 import os
 
@@ -28,6 +29,9 @@ app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
 app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
 app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+# Inicializa MySQL
+init_db(app)
 
 # Inicializar extensiones
 mysql = MySQL(app)
